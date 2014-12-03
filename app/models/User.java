871b4275af438dc0211e8b1fea1cc42fa1998c3f
Maik Wandrei <maik_546@yahoo.de>
2014-12-03 11:32:24 +0100

    Created evolution file for setting up database, created first database (will be manipulated in the next days) and created first model user.

---
+++ app/models/User.java
@@ -0,0 +1,108 @@
+package models;
+
+import play.data.format.Formats;
+import play.data.validation.Constraints;
+import play.db.ebean.Model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+
+
+/**
+ * Created by Maik Wandrei on 27.11.2014.
+ */
+@Entity
+public class User extends Model
+{
+    @Id
+    public Long id;
+
+    @Constraints.Required
+    @Formats.NonEmpty
+    @Column(unique = true)
+    public String e_mail;
+
+    @Constraints.Required
+    @Formats.NonEmpty
+    @Column(unique = true)
+    public String nickname;
+
+    @Constraints.Required
+    @Formats.NonEmpty
+    public String password_salt;
+
+    @Constraints.Required
+    @Formats.NonEmpty
+    public String password_hash;
+
+    @Constraints.Required
+    @Formats.NonEmpty
+    public Date time_creation;
+
+    @Constraints.Required
+    @Formats.NonEmpty
+    public Date time_password;
+
+    // -- Queries (long id, user.class)
+    public static Model.Finder<Long, User> find = new Model.Finder<Long, User>(Long.class, User.class);
+
+    /**
+     * Retrieve a user from an email.
+     *
+     * @param email email to search
+     * @return a user
+     */
+    public static User findByEmail(String email) {
+        return find.where().eq("email", email).findUnique();
+    }
+
+    /**
+     * Retrieve a user from an email.
+     *
+     * @param id id to search
+     * @return a user
+     */
+    public static User findById(Long id) {
+        return find.where().eq("id", id).findUnique();
+    }
+
+    /**
+     * Retrieve a user from a fullname.
+     *
+     * @param user_name Full name
+     * @return a user
+     */
+    public static User findByUsername(String user_name) {
+        return find.where().eq("user_name", user_name).findUnique();
+    }
+
+    /**
+     * Authenticate a User, from a email and clear password.
+     *
+     * @param email         email
+     * @param clearPassword clear password
+     * @return User if authenticated, null otherwise
+     * @throws AppException App Exception
+     */
+   /* public static User authenticate(String email, String clearPassword) throws AppException {
+
+        // get the user with email only to keep the salt password
+        User user = find.where().eq("email", email).findUnique();
+        if (user != null) {
+            // get the hash password from the salt + clear password
+            if (Hash.checkPassword(clearPassword, user.passwordHash)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public void changePassword(String password) throws AppException {
+        this.passwordHash = Hash.createPassword(password);
+        this.save();
+    }
+     */
+
+}
---
+++ conf/evolutions/1.sql
@@ -0,0 +1,52 @@
+  # --- !Ups
+CREATE TABLE user
+  (
+    id                bigint not null,
+    e_mail            varchar(255) not null unique,
+    nickname          varchar(50) not null unique,
+    password_salt     varchar(30) not null,
+    password_hash     varchar(30) not null,
+    time_creation     DATETIME not null,
+    time_password     DATETIME not null,
+    PRIMARY KEY (id)
+  );
+
+CREATE TABLE object
+  (
+    id                bigint not null unique,
+    game_id           bigint not null,
+    name              varchar(255) not null,
+    old               boolean not null,
+    usages_left       int,
+    item_id           bigint,
+    PRIMARY KEY (id)
+  );
+
+CREATE TABLE game
+  (
+    id                bigint not null unique,
+    user_id           bigint not null,
+    start_time        DATETIME not null,
+    completed         DATETIME,
+    PRIMARY KEY (id)
+  );
+
+CREATE TABLE char
+  (
+    id                bigint not null unique,
+    game_id           bigint not null unique,
+    name              varchar(255) not null,
+    old               boolean not null,
+    action_points     int not null,
+    position          varchar(255) not null,
+    PRIMARY KEY (id)
+  );
+
+# --- !Downs
+DROP TABLE user;
+
+DROP TABLE object;
+
+DROP TABLE game;
+
+DROP TABLE char;
