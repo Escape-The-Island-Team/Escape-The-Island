ccbce935bf0e52d904a5cbf277b50ab592b1a331
Vanessa Scheib <scheib.vanessa@gmx.de>
2014-12-04 17:06:41 +0100

    Added Terms of Use page 2.0

--- public/javascripts/selection.js
+++ public/javascripts/selection.js
@@ -4,12 +4,12 @@ function select(name) {
 
     switch (name) {
         case "Bob":
-            document.getElementById('description').value='Bob (20 Years) is a normal business administration student from Germany. His grades are like the average. He likes to eat bananas and hates coconuts. He is not very strong but sometimes he has his special moments, there he is very smart. Some years ago he had a girlfriend, but that friendship was not long. Because he is very pessimistic and heartless.';
+            document.getElementById('description').value='Bob (20 Years) is a normal business administration student from Germany. His grades are like the average. He likes to eat bananas and hates coconuts. He is not very strong but he has his moments and becomes very smart. Some years ago he had a girlfriend, but not for long. Because he is very pessimistic and heartless.';
             document.getElementById('Bob').style.border = "2px solid red";
             break;
 
         case "Alice":
-            document.getElementById('description').value='A strong and beautiful woman, who is the captain of a very dangerous pirate crew. After a heavy strorm her ship was damaged and she had lost her crew but not her faithful pet CuCu.';
+            document.getElementById('description').value='A strong and beautiful woman, who is the captain of a very dangerous pirate crew. After a heavy storm has passed, her ship was damaged and she had lost her crew but not her faithful pet CuCu.';
             document.getElementById('Alice').style.border = "2px solid red";
             break;
     }
