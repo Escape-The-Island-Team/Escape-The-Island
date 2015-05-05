379470c05f3dc73f2d54529c5812ae1108ea22a7
alisaIT <alisa-koksjuk@gmx.de>
2015-05-05 16:27:16 +0200

    Class NewNpcLister
    Created another JUnit test NewNpcListerTest

--- test/controllersTest/NewNpcListerTest.java
+++ test/controllersTest/NewNpcListerTest.java
@@ -1,19 +1,23 @@
 package controllersTest;
 
 import controllers.LocationParser;
+import controllers.Locationcontent.NewNpcLister;
 import junit.framework.TestCase;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class NewNpcListerTest extends TestCase {
 
     public void testGetNpcs() throws Exception {
 
-        List<String> list = LocationParser.getObjects("beachMid", true);
+        List<String> result = new NewNpcLister().getNpcs("tempel");
 
-        String actual = list.get(0);
 
-        String expected = "oldStone";
+        String actual = result.get(0);
+        assertEquals(1,result.size());
+
+        String expected = "nativeMaya";
 
         assertEquals(expected, actual);
     }
