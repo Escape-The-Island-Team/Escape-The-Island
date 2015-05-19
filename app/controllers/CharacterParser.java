1de9d89a8a764529817aa34ae694812444a328ab
Maik Wandrei <maik_546@yahoo.de>
2015-05-19 16:04:20 +0200

    ISLAND-235 - Create java classes for interacting with objects
    - finally correctly display the selected character
    - changed the boolean values in character to int (play/ebean reasons)
    - character says his/her 4th text

--- app/controllers/CharacterParser.java
+++ app/controllers/CharacterParser.java
@@ -7,53 +7,73 @@ import java.security.InvalidParameterException;
  */
 public class CharacterParser
 {
-    public static boolean isCharacter(String potentialCharacter)
+    public static int isCharacter(String character)
     {
-        if (potentialCharacter.equals("Alice"))
-        {
-            return true;
-        }
-
-        if (potentialCharacter.equals("Bob"))
-        {
-            return true;
-        }
-
-        return false;
-    }
-
-    public static boolean isOld(String character)
-    {
-        if (character.equals("Alice"))
-        {
-            return true;
-        }
-
         if (character.equals("Bob"))
         {
-            return false;
+            return 1;
         }
 
-        if (character.equals("CaptainSpeckJarrow"))
+        if (character.equals("Alice"))
         {
-            return true;
-        }
-
-        if (character.equals("BerryStraw"))
-        {
-            return false;
+            return 1;
         }
 
         if (character.equals("Nova"))
         {
-            return false;
+            return 1;
         }
 
         if (character.equals("HomTanks"))
         {
-            return false;
+            return 1;
         }
 
-        throw new InvalidParameterException("UnknownCharacter");
+        if (character.equals("BerryStraw"))
+        {
+            return 1;
+        }
+
+        if (character.equals("CaptainSpeckJarrow"))
+        {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    public static int isOld(String character)
+    {
+        if (character.equals("Bob"))
+        {
+            return 0;
+        }
+
+        if (character.equals("Alice"))
+        {
+            return 1;
+        }
+
+        if (character.equals("Nova"))
+        {
+            return 0;
+        }
+
+        if (character.equals("HomTanks"))
+        {
+            return 0;
+        }
+
+        if (character.equals("BerryStraw"))
+        {
+            return 0;
+        }
+
+        if (character.equals("CaptainSpeckJarrow"))
+        {
+            return 1;
+        }
+
+        return -1;
     }
 }
